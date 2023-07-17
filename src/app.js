e9dfb0856d3de69039dcae9099c1a3f7db52ed6c
Domenic Horner <domenic@tgxn.net>
2023-07-17 21:29:43 +0800

    add readme and app

--- src/app.js
+++ src/app.js
@@ -43,21 +43,6 @@ app.once("ready", () => {
 
   autoUpdater.checkForUpdatesAndNotify();
 
-  autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-      type: "info",
-      buttons: ["Restart", "Later"],
-      title: "Application Update",
-      message: process.platform === "win32" ? releaseNotes : releaseName,
-      detail:
-        "A new version has been downloaded. Restart the application to apply the updates.",
-    };
-
-    dialog.showMessageBox(dialogOpts).then((returnValue) => {
-      if (returnValue.response === 0) autoUpdater.quitAndInstall();
-    });
-  });
-
   const loadUrl = localUrl();
   mainWindow.loadURL(loadUrl);
 
